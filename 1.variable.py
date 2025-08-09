1a05ac438c47945c54512b0be2d901ff42595c89
sehoon0409 <20090409ksh@gmail.com>
2025-08-09 11:20:56 +0900

    기본 데이터 형태 학습

--- 1.variable.py
+++ 1.variable.py
@@ -28,3 +28,37 @@ print(x[0])
 x = {'a':1, 'b':3} #dictionary  key:value
 print(x)
 print(x['a'])
+#정오표
+example = {
+    'python': [True, False, True, True, True, True, True, False, False, True],
+    'java': [True, False, False, True, True, True, False, False, False, True],
+    'git': [False, False, True, True, False, True, True, True, True, True],
+}
+
+
+print(example)
+print(example['python'])
+print(example['python'][1])
+#문제 답안
+python_description = [
+    {
+        'answer': 1,
+        'description': 'python에 대한 설명은 1번이 맞습니다.'
+    },
+    {
+        'answer': "list",
+        'description': 'python의 열거형 데이터 타입은 list입니다.'
+    },
+    {
+        'answer': True,
+        'description': 'python의 LIST 안에 Dictionary를 사용할 수 있습니다.'
+    },
+]
+
+print(python_description[0])
+
+
+list_example = [1, "+", 2, "="]
+
+print(list_example)
+print(list_example[0])
