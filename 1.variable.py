62bc636bfd40ed6780025147cd8bb11b511a0b2b
sehoon0409 <20090409ksh@gmail.com>
2025-08-09 11:11:54 +0900

    기본 데이터 형태 학습

---
+++ 1.variable.py
@@ -0,0 +1,30 @@
+x = 100 #x에 숫자형 100을 대입함
+print(x)
+
+x = 100
+print(x + 200)
+
+x = "안녕"
+y = x +str(3)
+
+print(y)
+
+x = 3 == 5 # 3==5 가 거짓임 으로 boolean 값인 False를 x에 대입함
+
+print(x)
+
+x = "안녕" #x에 문자형 "안녕"을 대입함
+print(x)
+print(type(x))
+
+x = True   #x에 boolean값 True 대입함
+print(x)
+print(type(x))
+
+x = [1,2,3,4,5,6,7] #list
+print(x)
+print(x[0])
+
+x = {'a':1, 'b':3} #dictionary  key:value
+print(x)
+print(x['a'])
