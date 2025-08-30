46a6fe6722e319c1dafd1cbaba46b20a08841db0
sehoon0409 <20090409ksh@gmail.com>
2025-08-30 09:30:47 +0900

    숫자에 대한 연산

---
+++ 2.operater.py
@@ -0,0 +1,35 @@
+x=6 #대입 연산자
+y=4
+
+#조건 연산자
+z= x == y
+print("x==y : ", z)
+z= x!= y
+print("x! =y : ", z)
+z= x >  y
+print("x >y : ", z)
+z= x < y
+print("x <y : ", z)
+z= x >= y
+print("x >= y : ", z)
+z= x <= y
+print("x <= y : ", z)
+
+#사칙연산
+z = x + y
+print("x + y : " , z)
+z = x - y
+print("x - y : " , z)
+z = x * y
+print("x * y : " , z)
+z = x / y
+print("x / y : " , z)
+
+z = x % y
+print("x % y : " , z)
+z = x // y
+print("x // y : " , z)
+
+z = x ** y
+print("x ** y : " , z)
+
