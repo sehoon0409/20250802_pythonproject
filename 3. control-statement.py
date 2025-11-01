6212a78bb6072a4e789859147b335fe8385931a2
sehoon0409 <20090409ksh@gmail.com>
2025-11-01 11:14:54 +0900

    control-statement

--- 3. control-statement.py
+++ 3. control-statement.py
@@ -1,10 +1,13 @@
 score = int(input("점수를 입력하세요."))
 
-if score <= 100:
-    if score >= 0:
-        if score >= 80: print("합격입니다.")
-        elif score >= 60:
-            print("재시험 응시가 필요합니다.")
-        else:
-            print("불합격입니다.")
+if score > 100 or score < 0:
+    print("정상적인 점수 범위가 아닙니다.")
+else:
+    if score >= 80:
+        print("합격입니다.")
+    elif score >= 60:
+        print("재시험 응시가 필요합니다.")
+    else:
+        print("불합격입니다.")
+
 
